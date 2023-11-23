5a7407358821ca989938e62f13d1f2371369f3a2
Daniel Sikora <danielsikora100@gmail.com>
2023-11-23 12:50:13 +0100

    Dodanie filtowania po rozmiarze buta

--- butik/src/app/components/butik/butik.component.ts
+++ butik/src/app/components/butik/butik.component.ts
@@ -12,6 +12,7 @@ export class butikComponent implements OnInit {
   public filteredItems$: any;
   public minPrice: number | undefined;
   public maxPrice: number | undefined;
+  public shoeSize: number | undefined;
 
   constructor(private service: DataService) {}
 
@@ -29,19 +30,36 @@ export class butikComponent implements OnInit {
 
   applyFilter() {
     if (this.items$) {
-      this.filteredItems$ = this.items$.filter((item: any) => {
-        const titleMatch = item.title.toLowerCase().includes(this.filterText.toLowerCase());
-        const priceMatch = (this.minPrice === undefined || item.price >= this.minPrice) &&
-                           (this.maxPrice === undefined || item.price <= this.maxPrice);
-        return titleMatch && priceMatch;
-      });
+      let filteredItems = this.items$;
+  
+      const isAnyFilterUsed =
+        this.filterText !== '' || this.minPrice !== undefined || this.maxPrice !== undefined || this.shoeSize !== undefined;
+  
+      if (isAnyFilterUsed) {
+        filteredItems = this.items$.filter((item: any) => {
+          const titleMatch = item.title.toLowerCase().includes(this.filterText.toLowerCase());
+          const priceMatch = (this.minPrice === undefined || item.price >= this.minPrice) &&
+                             (this.maxPrice === undefined || item.price <= this.maxPrice);
+          const sizeMatch = (this.shoeSize === undefined || item.rozmiar === this.shoeSize);
+          return titleMatch && priceMatch && sizeMatch;
+        });
+      }
+  
+      this.filteredItems$ = filteredItems;
     } else {
       this.filteredItems$ = this.items$;
     }
   }
-
   
   onFilterTextChange() {
+    this.applyFilter();
+  }
+  
+  clearFilters() {
+    this.filterText = '';
+    this.minPrice = undefined;
+    this.maxPrice = undefined;
+    this.shoeSize = undefined;
     this.applyFilter();
   }
 }
