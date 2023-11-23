2c1302747f035d0f00fda6b285f8e1bfef1c68e4
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-11-23 11:24:50 +0100

    Dodanie odliczania po zakupie.

--- butik/src/app/components/buy-form/buy-form.component.ts
+++ butik/src/app/components/buy-form/buy-form.component.ts
@@ -1,15 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import {DataService} from "../../services/data.service";
+import { DataService } from "../../services/data.service";
 
 @Component({
   selector: 'app-buy-form',
   templateUrl: './buy-form.component.html',
   styleUrls: ['./buy-form.component.css']
 })
-export class BuyFormComponent {
+export class BuyFormComponent implements OnInit {
   isFormValid: boolean = false;
+  public purchaseMessage: string = '';
+  public countdown: number = 15;
 
   public title: string = '';
   public image: string = '';
@@ -18,29 +20,38 @@ export class BuyFormComponent {
   public price: number = 0;
   public id: string = '';
 
-  constructor(private service: DataService, private route: ActivatedRoute, private authService: AuthService, public router: Router) {
-  }
+  constructor(
+    private service: DataService,
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    public router: Router
+  ) {}
 
   ngOnInit() {
-    let id: string = '';
-    this.route.paramMap
-      .subscribe((params: any) => {
-        id = params.get('id');
+    this.route.paramMap.subscribe((params: any) => {
+      const id = params.get('id');
+      this.service.getById(id).subscribe((res: any) => {
+        this.image = res['image'];
+        this.title = res['title'];
+        this.text = res['text'];
+        this.rozmiar = res['rozmiar'];
+        this.price = res['price'];
+        this.id = res['id'];
       });
-
-    this.service.getById(id).subscribe((res: any) => {
-      this.image = res['image'];
-      this.title = res['title'];
-      this.text = res['text'];
-      this.rozmiar = res['rozmiar'];
-      this.price = res['price'];
-      this.id = res['id'];
-
     });
 
+    const timer = setInterval(() => {
+      if (this.countdown > 0) {
+        this.countdown--;
+      } else {
+        clearInterval(timer);
+        this.router.navigate(['/butik']);
+      }
+    }, 1000); 
   }
 
   checkFormValidity() {
+    
     const formInputs = document.querySelectorAll<HTMLInputElement>('#myForm input[required]');
     this.isFormValid = true;
 
@@ -52,12 +63,10 @@ export class BuyFormComponent {
   }
 
   onSubmit() {
-    if(this.authService.isLoggedIn()) {
-      this.service.deletePost(this.id).subscribe((result) => {
-        return result;
+    if (this.authService.isLoggedIn()) {
+      this.service.deletePost(this.id).subscribe(() => {
+        this.purchaseMessage = 'Buciki zamówione!';
       });
-      this.router.navigate(['/butik']);
     }
-    
   }
 }
