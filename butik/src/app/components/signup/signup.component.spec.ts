e36be610385bdb19d95381b6fd439a1d0ce51aea
Daniel Sikora <danielsikora100@gmail.com>
2023-12-14 10:20:54 +0100

    testy integracyjne dla rejestracji

---
+++ butik/src/app/components/signup/signup.component.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SignupComponent } from './signup.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
+
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
+
+  beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['createOrUpdate']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      declarations: [SignupComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SignupComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;  
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call createOrUpdate on AuthService and navigate to "/" on successful creation', () => {
+    // given
+    const successResponse = { };
+    authService.createOrUpdate.and.returnValue(of(successResponse));
+
+    // when
+    component.create();
+
+    // then
+    expect(authService.createOrUpdate).toHaveBeenCalledWith(component.credentials);
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
+  });
+  
+});
