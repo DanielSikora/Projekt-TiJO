d7af5eb73a7bad18f6b51973b541b959e0830a60
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-12-17 10:52:02 +0100

    test integracyjne

--- Api/app/tests/post.endpoint.test.js
+++ Api/app/tests/post.endpoint.test.js
@@ -27,29 +27,29 @@ describe('Create post endpoint', () => {
   it('should retrieve all posts and respond with 200 status code', async () => {
     // Given
   
-    // When 
+    // When - wykonanie operacji pobrania wszystkich postów
     const response = await request(app)
       .get('/api/posts');
   
-    // Then 
+    // Then - weryfikacja odpowiedzi
     expect(response.status).toBe(200);
-    expect(response.body).toBeDefined(); 
-    expect(Array.isArray(response.body)).toBeTruthy(); 
+    expect(response.body).toBeDefined(); // Sprawdź, czy ciało odpowiedzi jest zdefiniowane
+    expect(Array.isArray(response.body)).toBeTruthy(); // Sprawdź, czy ciało odpowiedzi jest tablicą
     
   });
   it('should retrieve a single post and respond with 200 status code', async () => {
-    // Given - istniejący post
+    // Given - przygotowanie danych (np. znalezienie istniejącego posta)
   
-    // When 
+    // When - wykonanie operacji pobrania pojedynczego postu
     const response = await request(app)
       .get('/api/posts/55f3f2ce800e141f21ddf65'); 
   
-    // Then 
+    // Then - weryfikacja odpowiedzi
     expect(response.status).toBe(200);
     
   });
   it('should retrieve a single post', async () => {
-    // Given istneijący post
+    // Given 
     // When
     const response = await request(app)
       .get('/api/posts/655f3e50e800e141f21ddf4e');
@@ -62,7 +62,7 @@ describe('Create post endpoint', () => {
 
     // When
     const updatedData = {
-      
+      // dane do aktualizacji posta, użyj nieistniejącego ID lub innych danych
     };
     const response = await request(app)
       .put('/api/posts/wadasdad')
@@ -70,7 +70,7 @@ describe('Create post endpoint', () => {
   
     // Then
     expect(response.status).toBe(404);
-    
+    // Oczekujemy statusu 404, ponieważ nie ma posta do aktualizacji
 
   });
   it('should handle errors for creating a post without data', async () => {
@@ -78,7 +78,7 @@ describe('Create post endpoint', () => {
 
     // When
     const incompletePostData = {
-      
+      // Brak jednego lub więcej pól wymaganych
     };
     const response = await request(app)
       .post('/api/posts')
@@ -86,7 +86,7 @@ describe('Create post endpoint', () => {
 
     // Then
     expect(response.status).toBe(400);
-    
+    // Oczekujemy statusu 400, ponieważ dane są niekompletne
 
   });
   it('should handle errors for updating a post with invalid ID', async () => {
@@ -94,7 +94,7 @@ describe('Create post endpoint', () => {
 
     // When
     const updatedData = {
-      
+      // dane do aktualizacji posta, użyj nieprawidłowego ID lub innych danych
     };
     const response = await request(app)
       .put('/api/posts/invalid_id')
@@ -102,7 +102,7 @@ describe('Create post endpoint', () => {
   
     // Then
     expect(response.status).toBe(404);
-    
+    // Oczekujemy statusu 404, ponieważ podane ID jest nieprawidłowe
 
   });
   it('should handle errors for deleting without ID', async () => {
@@ -140,16 +140,172 @@ describe('Create post endpoint', () => {
 
     // Then
     expect(response.status).toBe(400);
-    
+    // Oczekujemy statusu 400, ponieważ dane są niekompletne
 
   });
 
   
-  
- 
+  it('should retrieve posts sorted by date in descending order', async () => {
+    // Given - pobieranie postów posortowanych według daty w kolejności malejącej
 
+    // When
+    const response = await request(app)
+      .get('/api/posts?sortBy=date&order=desc');
+
+    // Then
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+    expect(Array.isArray(response.body)).toBeTruthy();
+    // Dodatkowo sprawdzamy, czy posty są posortowane poprawnie
+    const dates = response.body.map(post => new Date(post.date));
+    expect(dates).toEqual([...dates].sort((a, b) => b - a));
+  });
+
+  it('should create a post with valid data and respond with 200 status code', async () => {
+    // Given
+    const postData = {
+      title: 'New post',
+      image: 'new-post.jpg',
+      text: 'This is a new test post',
+      rozmiar: 12,
+      price: 30,
+    };
   
+    // When
+    const response = await request(app)
+      .post('/api/posts')
+      .send(postData);
   
+    // Then
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined(); 
+    expect(response.body).toBeTruthy(); 
+  });
+ 
   
+  it('should handle errors for deleting without post and ID', async () => {
+    // Given - nieistniejące ID posta
+
+    // When
+    const response = await request(app)
+      .delete('/api');
+  
+    // Then
+    expect(response.status).toBe(404);
+    
+  });
+
+  it('should delete all shoes with size 52', async () => {
+    // Given
+    const responseGet = await request(app)
+      .get('/api/posts')
+      .query({ rozmiar: 52 });
+
+    // When
+    expect(responseGet.status).toBe(200);
+    expect(responseGet.body).toBeInstanceOf(Array);
+
+    // Then
+    const shoesToDelete = responseGet.body;
+    const deletePromises = shoesToDelete.map((shoe) =>
+    request(app).delete(`/api/posts/${shoe._id}`)
+    );
+  });
+ 
+  it('should retrieve all posts and respond with 200 status code', async () => {
+    // Given
+  
+    // When - wykonanie operacji pobrania wszystkich postów
+    const response = await request(app)
+      .get('/api/posts');
+  
+    // Then - weryfikacja odpowiedzi
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined(); 
+    expect(Array.isArray(response.body)).toBeTruthy();
+    
+  });
+
+  it('should handle errors for deleting a specific post by ID', async () => {
+    // Given - istniejące ID posta (np. ID równa 10)
+  
+    // When
+    const postIdToDelete = '10'; // Załóżmy, że post o ID 10 istnieje
+    const response = await request(app)
+      .delete(`/api/posts/${postIdToDelete}`);
+  
+    // Then
+    expect(response.status).toBe(400);
+  });
+  
+  it('should handle errors for deleting posts within the range of IDs 10 to 15', async () => {
+    // Given - zakres ID postów (od 10 do 15)
+  
+    const startId = 10;
+    const endId = 15;
+  
+    // When - usuń posty dla każdego ID w zakresie
+    for (let postId = startId; postId <= endId; postId++) {
+      const response = await request(app)
+        .delete(`/api/posts/${postId}`);
+  
+      // Then - oczekujemy statusu 400 dla każdego usunięcia
+      expect(response.status).toBe(400);
+    }
+  });
+  
+  it('should handle errors for creating a post with invalid price', async () => {
+    // Given - nieprawidłowe dane, gdzie price nie jest liczbą
+    const postData = {
+      title: 'Test Post',
+      image: 'test-image.jpg',
+      text: 'This is a test post',
+      rozmiar: 10,
+      price: 'InvalidPrice', // price ustawione jako ciąg znaków
+    };
+  
+    // When - próba utworzenia posta
+    const response = await request(app)
+      .post('/api/posts')
+      .send(postData);
+  
+    // Then - sprawdzenie, czy otrzymano odpowiedni błąd
+    expect(response.status).toBe(500);
+  });
+  
+  it('should handle errors for creating a post with invalid price', async () => {
+    // Given - nieprawidłowe dane, gdzie price nie jest liczbą
+    const postData = {
+      title: 'Test Post',
+      image: 'test-image.jpg',
+      text: 'This is a test post',
+      rozmiar: 'InvalidPrice', // invalid
+      price: 50, 
+    };
+  
+    // When - próba utworzenia posta
+    const response = await request(app)
+      .post('/api/posts')
+      .send(postData);
+  
+    // Then - sprawdzenie, czy otrzymano odpowiedni błąd
+    expect(response.status).toBe(500);
+  });
+  
+  it('should handle errors for creating a post not in the correct place', async () => {
+    // Given - niekompletne dane posta
+
+    // When
+    const incompletePostData = {
+      title: 'Test Post',
+    };
+    const response = await request(app)
+      .post('/api')
+      .send(incompletePostData);
+
+    // Then
+    expect(response.status).toBe(404);
+
+  });
   
 });
